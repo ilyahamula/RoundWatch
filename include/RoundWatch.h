62547966feaec1b6297cdb4e4009316aad3047ce
ilyahamula <ilyahamula95@gmail.com>
2020-06-20 00:19:08 +0300

    small interface fixes, add cpp file with partly implemented methods

--- include/RoundWatch.h
+++ include/RoundWatch.h
@@ -4,6 +4,8 @@
 #include "TimeLed.h"
 #include "RoundDial.h"
 
+#define INT(name) static_cast<int>(name)
+
 enum class DIAL
 {
     HOURS = 0,
@@ -21,29 +23,30 @@ public:
     // Dial`s interface
     void SetMottorPins(const DIAL dial, const uint8_t in1, const uint8_t in2, const uint8_t in3, const uint8_t in4);
     void SetNumDivisions(const DIAL dial, const uint8_t divisions);
+    void SetMajorDivisions(const DIAL dial, int n, int* array);
     void SetStopperPin(const DIAL dial, const uint8_t pin);
 
-    void Setup();
     // LED`s interface
+    void SetLedPin(const DIAL dial, const uint8_t pin);
     void SetTopBrightness(const DIAL dial, const double brightness); // max 1.0
     void SetBottomBrightness(const DIAL dial, const double brightness); // max 1.0
     void SetTopColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b);
     void SetBottomColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b); 
 
     // RoundWatch interface
-    void SetMajorDivisions(const DIAL dial, int n, int* array);
+    void Setup();
     void SetTime(const int16_t hour, const int16_t min = -1, const int16_t sec = -1);
 
 private:
-    static const int num = static_cast<int>(DIAL::NUM_DIALS);
+    static const int num = INT(DIAL::NUM_DIALS);
 
 private:
     RoundDial* m_dials[num];
-    TimeLed m_leds[num];
-    uint16_t m_timeValue[num];
+    TimeLed* m_leds[num];
+    bool* m_majorDiv[num];
 
 private:
-    bool* m_majorDiv[num];
+    uint16_t m_timeValue[num];
 };
 
 #endif  
