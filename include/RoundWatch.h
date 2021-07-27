c7a40223b2d7c14c563308a6bc98bb4490dd6b2a
ilyahamula <ilyahamula95@gmail.com>
2021-07-27 22:57:47 +0300

    Remove duplicated files and move changes to the original files

--- include/RoundWatch.h
+++ include/RoundWatch.h
@@ -1,5 +1,5 @@
 #ifndef ROUNWATCH_H
-#define  ROUNWATCH_H
+#define ROUNWATCH_H
 
 #include "TimeLed.h"
 #include "RoundDial.h"
@@ -9,19 +9,34 @@
 class RoundWatch
 {
 public:
+    struct LED
+    {
+        uint8_t red = 0;
+        uint8_t green = 0;
+        uint8_t blue = 0;
+        double brightness = 0.0;
+    };
+
+    struct Settings
+    {
+        // Mottor's pins
+        uint8_t in1 = 0;
+        uint8_t in2 = 0;
+        uint8_t in3 = 0;
+        uint8_t in4 = 0;
+
+        uint8_t divisionPin = 0;
+
+        //Led
+        uint8_t ledPin = 0;
+        LED topLED;
+        LED bottomLED;
+    };
+public:
     RoundWatch();
     ~RoundWatch();
 
-    // Dial`s interface
-    void SetMottorPins(const DIAL dial, const uint8_t in1, const uint8_t in2, const uint8_t in3, const uint8_t in4);
-    void SetDivisionsPin(const DIAL dial, const uint8_t pin);
-
-    // LED`s interface
-    void SetLedPin(const DIAL dial, const uint8_t pin);
-    void SetTopBrightness(const DIAL dial, const double brightness); // max 1.0
-    void SetBottomBrightness(const DIAL dial, const double brightness); // max 1.0
-    void SetTopColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b);
-    void SetBottomColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b); 
+    void AdjustDialSettings(const DIAL dial, const Settings& settings);
 
     // RoundWatch interface
     void Setup();
@@ -32,6 +47,18 @@ public:
     void MoveForward(const DIAL dial);
     void MoveBackward(const DIAL dial);
     void MoveOneDivForward(const DIAL dial);
+
+private:
+    // Dial`s interface
+    void SetMottorPins(const DIAL dial, const uint8_t in1, const uint8_t in2, const uint8_t in3, const uint8_t in4);
+    void SetDivisionsPin(const DIAL dial, const uint8_t pin);
+
+    // LED`s interface
+    void SetLedPin(const DIAL dial, const uint8_t pin);
+    void SetTopBrightness(const DIAL dial, const double brightness); // max 1.0
+    void SetBottomBrightness(const DIAL dial, const double brightness); // max 1.0
+    void SetTopColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b);
+    void SetBottomColor(const DIAL dial, const uint8_t r, const uint8_t g, const uint8_t b); 
 
 private:
     static const int num = INT(DIAL::NUM_DIALS);
